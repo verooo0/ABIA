d478291e6cc257af64dc4074c88ba2b95a443253
claudia <150239584+gallego-c@users.noreply.github.com>
2024-11-09 01:21:35 +0100

    Add files via upload
    falla que dejarlo vacio no es possible, almenos como lo estoy haciendo (metiendole lista vacio), porque  entra en conflicto confunciones que luego usan el assignacions, lo pongo por si se os pcurre algo pa solucionarlo

--- intento/bin_packing_state_opt_modified.py
+++ intento/bin_packing_state_opt_modified.py
@@ -1,5 +1,5 @@
 from typing import List, Dict, Generator
-from bin_packing_operators_modified import AzamonOperator, AssignPackage, SwapAssignments, EliminatePackage
+from bin_packing_operators_modified import AzamonOperator, AssignPackage, SwapAssignments, RemovePackage
 from bin_packing_problem_parameters_modified import AzamonParameters
 
 class StateRepresentation(object):
@@ -9,6 +9,7 @@ class StateRepresentation(object):
         self.v_p = assignments
         self.happiness = happiness or {}
         self.update_happiness()
+        self.falta = []
 
     def update_happiness(self):
         for pkg_id, offer_id in enumerate(self.assignments):
@@ -39,8 +40,17 @@ class StateRepresentation(object):
 
         for pkg_id_1 in range(len(self.assignments)):
             for pkg_id_2 in range(len(self.assignments)):
-                if pkg_id_1 != pkg_id_2:
+                if pkg_id_1 != pkg_id_2 and self.assignments[pkg_id_1] != self.assignments[pkg_id_2] :
                     yield SwapAssignments(pkg_id_1, pkg_id_2)
+
+########################################
+
+
+        for pkg_id, offer_id in enumerate(self.assignments):
+            if pkg_id not in self.falta:
+                yield RemovePackage(pkg_id, offer_id)
+
+            
 
     
         
@@ -56,34 +66,42 @@ class StateRepresentation(object):
             pkg_id_2 = action.package_id_2
             new_state.assignments[pkg_id_1], new_state.assignments[pkg_id_2] = new_state.assignments[pkg_id_2], new_state.assignments[pkg_id_1]
             new_state.update_happiness()
+        elif isinstance(action, RemovePackage):
+            pkg_id = action.package_id
+            new_offer_id = action.offer_id
+            new_state.assignments[pkg_id] = []
+            self.falta.append(pkg_id)
         return new_state
 
     def heuristic_cost(self) -> float:
         total_transport_cost, total_storage_cost= 0.0, 0.0
         #send_cost = sum(self.params.price_kg[offer_id] * self.params.package_weights[pkg_id] for pkg_id, offer_id in enumerate(self.assignments))
         for pkg_id, offer_id in enumerate(self.assignments):
-            package_weight = self.params.package_weights[pkg_id]
-            days_limit = self.params.days_limits[offer_id]
-            price_per_kg = self.params.price_kg[offer_id]
 
-            # 1. Coste de transporte
-            transport_cost = price_per_kg * package_weight
-            total_transport_cost += transport_cost
+            #PROB xyz
+            if self.assignments[pkg_id] != []:
+                package_weight = self.params.package_weights[pkg_id]
+                days_limit = self.params.days_limits[offer_id]
+                price_per_kg = self.params.price_kg[offer_id]
 
-        # 2. Coste de almacenamiento (solo para ofertas con 3 días o más)
-        # Si el paquete está asignado a una oferta de 3 días o más, calculamos el coste de almacenamiento.
-            if days_limit >= 3:
-                if days_limit == 3 or days_limit == 4:
-                    storage_days = 1  # Paquetes de 3-4 días se almacenan un día
-                elif days_limit == 5:
-                    storage_days = 2  # Paquetes de 5 días se almacenan dos días
+                # 1. Coste de transporte
+                transport_cost = price_per_kg * package_weight
+                total_transport_cost += transport_cost
 
-                # Calculo del coste de almacenamiento basado en el peso del paquete y los días de almacenamiento
-                storage_cost = storage_days * 0.25 * package_weight
-                total_storage_cost += storage_cost
-        #return sum(self.params.offer_capacities[offer_id] * self.params.package_weights[pkg_id] for pkg_id, offer_id in enumerate(self.assignments))
-        #return sum(self.params.price_kg[offer_id] * self.params.package_weights[pkg_id] for pkg_id, offer_id in enumerate(self.assignments))
-        
+            # 2. Coste de almacenamiento (solo para ofertas con 3 días o más)
+            # Si el paquete está asignado a una oferta de 3 días o más, calculamos el coste de almacenamiento.
+                if days_limit >= 3:
+                    if days_limit == 3 or days_limit == 4:
+                        storage_days = 1  # Paquetes de 3-4 días se almacenan un día
+                    elif days_limit == 5:
+                        storage_days = 2  # Paquetes de 5 días se almacenan dos días
+
+                    # Calculo del coste de almacenamiento basado en el peso del paquete y los días de almacenamiento
+                    storage_cost = storage_days * 0.25 * package_weight
+                    total_storage_cost += storage_cost
+            #return sum(self.params.offer_capacities[offer_id] * self.params.package_weights[pkg_id] for pkg_id, offer_id in enumerate(self.assignments))
+            #return sum(self.params.price_kg[offer_id] * self.params.package_weights[pkg_id] for pkg_id, offer_id in enumerate(self.assignments))
+            
         return total_transport_cost + total_storage_cost
     
     def heuristic_happiness(self) -> float:
