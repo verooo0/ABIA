2177d82f5be613fa39c6fa5668e0138380deaea1
claudia <150239584+gallego-c@users.noreply.github.com>
2024-11-11 11:45:00 +0100

    Update bin_packing_problem_opt_modified.py exp1 automat

--- intento_fix/bin_packing_problem_opt_modified.py
+++ intento_fix/bin_packing_problem_opt_modified.py
@@ -5,16 +5,19 @@ from bin_packing_operators_modified import AzamonOperator
 from bin_packing_state_opt_modified import StateRepresentation
 
 class AzamonProblem(Problem):
-    def __init__(self, initial_state: StateRepresentation, maximize_happiness: bool = False, use_entropy=False, mode_simulated_annealing: bool = False, combine_heuristic: bool = False, alpha: float = 0.1):
+    def __init__(self, initial_state: StateRepresentation, maximize_happiness: bool = False, use_entropy=False, mode_simulated_annealing: bool = False, combine_heuristic: bool = False, alpha: float = 0.1, op1=True, op2=True, op3=False):
         self.maximize_happiness = maximize_happiness
         self.use_entropy = use_entropy
         self.mode_simulated_annealing = mode_simulated_annealing
         self.combine_heuristic = combine_heuristic
         self.alpha = alpha
+        self.op1 = op1
+        self.op2 = op2
+        self.op3 = op3
         super().__init__(initial_state)
 
     def actions(self, state: StateRepresentation) -> Generator[AzamonOperator, None, None]:
-        return state.generate_actions(self.mode_simulated_annealing)
+        return state.generate_actions_automatic(self.mode_simulated_annealing, self.op1, self.op2, self.op3)
 
     def result(self, state: StateRepresentation, action: AzamonOperator) -> StateRepresentation:
         return state.apply_action(action)
